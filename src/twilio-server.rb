a670efb5816a70b0556084b0086800757908e658
Akira Saito <akira.saito1986@gmail.com>
2017-08-26 05:40:05 +0900

    add twilio-server

--- src/twilio-server.rb
+++ src/twilio-server.rb
@@ -3,6 +3,34 @@ require 'sinatra'
 require 'twilio-ruby'
 require 'logger'
 
+SERVER_URL = "http://153.127.195.16:4567"
+
+post '/first-confirm' do
+  PERSON_COUNT = params["person_count"]
+  START_TIME = params["start_time"]
+  response = Twilio::TwiML::VoiceResponse.new do |r|
+    r.say("すみません。予約をお願いしたいのですが、#{START_TIME} 時から #{PERSON_COUNT} 人で空いていますか？", voice: 'woman', language: "ja-JP")
+    r.gather(action: "#{SERVER_URL}/shop_answer", method: 'GET') do |gather|
+      gather.say("予約可能であれば １を、できなければ３を押してください", voice: 'woman', language: "ja-JP")
+    end
+  end.to_s
+  logger.info(response)
+  response
+end
+
+get '/shop_answer' do
+  logger.info("shop_answer")
+  logger.info(params)
+  ANSWER_DIGITS = params["Digits"]
+  response = Twilio::TwiML::VoiceResponse.new do |r|
+    if ANSWER_DIGITS = "1"
+      r.say("予約させていただきました", voice: 'woman', language: "ja-JP")
+    else
+      r.say("予約できないとのことで、了解いたしました。", voice: 'woman', language: "ja-JP")
+    end
+  end.to_s
+end
+
 get '/callback-answer' do
   logger.info("testaaa") 
   logger.info(params)
@@ -16,13 +44,6 @@ get '/callback-answer' do
 end
 
 get '/hello-monkey' do
-  people = {
-    '+14158675309' => 'Curious George',
-    '+14158675310' => 'Boots',
-    '+14158675311' => 'Virgil',
-    '+14158675312' => 'Marcel',
-  }
-  name = people[params['From']] || 'Monkey'
   Twilio::TwiML::VoiceResponse.new do |r|
     r.say("Hello もとださん 二次会アプリケーション頑張って作りましょう", voice: 'woman', language: "ja-JP")
   end.to_s
